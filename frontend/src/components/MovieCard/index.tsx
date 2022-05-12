9dfa8db94d234ef4d0e1b5a6be79c5d96cba5faa
Bruno Kaique <brunokaique@gmail.com>
2022-05-12 15:20:12 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,6 +1,7 @@
 
 
 import MovieScore from 'components/MovieScore';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 function MovieCard() {
@@ -15,13 +16,16 @@ function MovieCard() {
 
     return (
         <div>
-        <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-        <div className="dsmovie-card-bottom-container">
-            <h3>{movie.title}</h3>
-            <MovieScore />
-            <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <MovieScore />
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
+
+            </div>
         </div>
-    </div>
     );
 
 }
--- frontend/src/page/Form/index.tsx
+++ frontend/src/page/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './style.css';
 function Form() {
 
@@ -10,7 +11,7 @@ function Form() {
     };
 
     return (
-        
+
         <div className="dsmovie-form-container">
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
@@ -34,7 +35,10 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+
             </div >
         </div >
 
