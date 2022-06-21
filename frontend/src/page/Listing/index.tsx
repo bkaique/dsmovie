fe5ea00c6615f6e5daa88156bdbd7d261644ea6d
Bruno Kaique <brunokaique@gmail.com>
2022-06-21 12:15:41 -0300

    useState, useEffect

--- frontend/src/page/Listing/index.tsx
+++ frontend/src/page/Listing/index.tsx
@@ -1,42 +1,62 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movies";
 import { BASE_URL } from "ultis/request";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies?size12&page=0`)
-    .then(resposta =>{
-        console.log(resposta.data) ;
+
+    const [PageNumber, setPageNumber] = useState(0);
+useEffect(() =>{
+
+    axios.get(`${BASE_URL}/movies?size12&page=1`)
+    .then(resposta => {
+        const data = resposta.data as MoviePage
+        setPageNumber(data.number);
     });
-    return (<><Pagination />
-        <div className="container">
-            <div className="row">
 
-                <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
-                    <MovieCard />
-                </div>
-                <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
-                    <MovieCard />
-                </div>
-                <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
-                    <MovieCard />
-                </div>
-                <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
-                    <MovieCard />
-                </div>
-                <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
-                    <MovieCard />
-                </div>
-                <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
-                    <MovieCard />
-                </div>
-                <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
-                    <MovieCard />
-                </div>
+},[])
 
-            </div>
-        </div>
 
-    </>
+   
+    return (
     
+                <>
+                    <p>{PageNumber}</p>
+                <Pagination />
+                <div className="container">
+                    <div className="row">
+
+                        <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                            <MovieCard />
+                        </div>
+
+                    </div>
+                </div>
+
+            </>
+            );
+
+}
+
+            export default Listing
